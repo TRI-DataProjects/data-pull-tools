b895caf5439fe995199b58ca65d52e0543535f17
John Merkel <dev.genericallyterrible@gmail.com>
2022-06-22 15:37:53 -0700

    Updated collect, moved to Pool

--- Collect.py
+++ Collect.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process, Queue, freeze_support
+from multiprocessing import Process, Queue, Pool
 import os
 import sys
 import time
@@ -8,89 +8,56 @@ from ArchiveCollector import *
 from CollectorMap import ARCHIVE_COLLECTORS, Month
 from PromptSpinner import *
 
+
 class CollectorSignal(Enum):
     DONE = 0
 
-collection_path = Path(os.path.dirname(os.path.realpath(sys.argv[0]))) / 'collected'
 
-def collect_all():
-    tot_start = time.time()
-    for year in ARCHIVE_COLLECTORS:
-        collect_year(year)
-    tot_end = time.time()
-    print(f'Operation completed ({tot_end - tot_start:.3f}s)')
+collection_path = Path(os.path.dirname(
+    os.path.realpath(sys.argv[0]))) / 'collected'
 
-def collect_year(year):
-    if (year in ARCHIVE_COLLECTORS):
-        year_start = time.time()
-        year_dict = ARCHIVE_COLLECTORS[year]
-        print(f'{year} Begin')
-        for month in year_dict:
-            collect(year, month)
-        year_end = time.time()
-        print(f'{year} End ({year_end - year_start:.3f}s)')
-        return
-    print(f'Year {year} not found in ARCHIVE_COLLECTORS dictionary')
 
-def collect(year, month, overwrite_existing=False):
+def collect_with_stats(year, month, overwrite_existing=False):
     if (year in ARCHIVE_COLLECTORS):
         year_dict = ARCHIVE_COLLECTORS[year]
         if (month in year_dict):
-            file_exists = os.path.isfile(collection_path / f'{year}-{month.value}.xlsx')
+            file_exists = os.path.isfile(
+                collection_path / f'{year}-{month.value}.xlsx')
             if (
                 (not file_exists) or
                 (file_exists and overwrite_existing)
             ):
-                print(f'{year}-{month.value} Begin')
+                print(f'Collecting {year}-{month.value}')
                 month_start = time.time()
-                year_dict[month].collect(collection_path, f'{year}-{month.value}')
+                df_head = year_dict[month].collect(
+                    collection_path, f'{year}-{month.value}')
                 month_end = time.time()
-                print(f'{year}-{month.value} End ({month_end - month_start:.3f}s)')
+                print(
+                    f'Collected {year}-{month.value} ({month_end - month_start:.3f}s)')
             else:
-                print(f'{year}-{month.value} already present')
-            return
-    print(f'Year-Month combination {year}-{month.value} not found in ARCHIVE_COLLECTORS dictionary')
-    return
+                print(f'Reading {year}-{month.value} to analyze columns')
+                df_head = pd.read_excel(
+                    collection_path / f'{year}-{month.value}.xlsx', nrows=0)
 
-def collector_proc(queue):
-    while True:
-        msg = queue.get()
-        if msg != CollectorSignal.DONE:
-            collect(msg['year'], msg['month'], msg['overwrite'])
-        else:
-            break
-
-def start_collector_procs(proc_queue, num_procs):
-    all_procs = list()
-    for i in range(0, num_procs):
-        col_proc = Process(target=collector_proc, args=(proc_queue,))
-        col_proc.daemon = True
-        col_proc.start()
-        all_procs.append(col_proc)
-    return all_procs
+            rows = []
+            for col in df_head.columns:
+                rows.append([col, year, month.value])
+            return pd.DataFrame(rows, columns=['Column Name', 'Year', 'Month'])
 
 
 if __name__ == '__main__':
-    num_procs = 6
-    proc_queue = Queue()
-    start_time = time.time()
-    # Build the queue
+    targets = []
+    overwrite = False
     for year in ARCHIVE_COLLECTORS:
         for month in ARCHIVE_COLLECTORS[year]:
-            msg = {
-                'year': year,
-                'month': month,
-                'overwrite': False,
-            }
-            proc_queue.put(msg)
-    # Create the procs
-    all_procs = start_collector_procs(proc_queue, num_procs)
-    # Add signals for each proc to terminate
-    for proc in enumerate(all_procs):
-        proc_queue.put(CollectorSignal.DONE)
+            targets.append([year, month, overwrite])
 
-    # Wait for procs to complete
-    for idx, proc in enumerate(all_procs):
-        proc.join()
+    print(f'Collecting archived data')
+    start_time = time.time()
+    with Pool() as pool:
+        dfs = pool.starmap(collect_with_stats, targets)
     end_time = time.time()
     print(f"Completed collection in {end_time-start_time:.3f}s")
+
+    data = pd.concat(dfs, ignore_index=True)
+    data.to_csv(collection_path / 'column_stats.csv', index=False)
