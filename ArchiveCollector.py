8bed97cf928e1a35f67a4aa66a2ebd04862e9edd
John Merkel <dev.genericallyterrible@gmail.com>
2022-06-22 10:57:19 -0700

    Update ArchiveCollector.py
    Removed prints for multithreading compatibility

--- ArchiveCollector.py
+++ ArchiveCollector.py
@@ -31,46 +31,13 @@ class ArchiveComposer(ArchiveCollector):
         self.address_sheet = address_sheet
 
     def collect(self, out_dir, out_file):
-        col_start = time.time()
-        # spinner = DotsSpinner()
-
-        # spinner.start_spin('Reading in general info')
-        # start = time.time()
+        
         general = self.read_excel(self.folder_root / (self.general_file + '.xlsx'), self.general_sheet)
-        # end = time.time()
-        # spinner.stop_spin()
-        # print(f'({end - start:.3f}s)')
-
-        # spinner.start_spin('Reading in care center info')
-        # start = time.time()
         care_center = self.read_excel(self.folder_root / (self.care_center_file + '.xlsx'), self.care_center_sheet)
-        # end = time.time()
-        # spinner.stop_spin()
-        # print(f'({end - start:.3f}s)')
-        
-        # spinner.start_spin('Reading in address info')
-        # start = time.time()
         address = self.read_excel(self.folder_root / (self.address_file + '.xlsx'), self.address_sheet)
-        # end = time.time()
-        # spinner.stop_spin()
-        # print(f'({end - start:.3f}s)')
-        
-        # spinner.start_spin('Merging the info')
-        # start = time.time()
         merged = general.merge(care_center, on='Provider UID', how='left').merge(address, on='Provider UID', how='left')
-        # end = time.time()
-        # spinner.stop_spin()
-        # print(f'({end - start:.3f}s)')
-        
-        # spinner.start_spin('Writing info to output file')
-        # start = time.time()
+
         merged.to_excel(out_dir / (out_file + '.xlsx'), index=False)
-        # end = time.time()
-        # spinner.stop_spin()
-        # print(f'({end - start:.3f}s)')
-        
-        col_end = time.time()
-        print(f'Data collection successful ({col_end - col_start:.3f}s)')
 
 class ArchiveCopier(ArchiveCollector):
     def __init__(self, folder_root, file_name, file_sheet=None):
@@ -79,22 +46,6 @@ class ArchiveCopier(ArchiveCollector):
         self.file_sheet = file_sheet
 
     def collect(self, out_dir, out_file):
-        col_start = time.time()
-        # spinner = DotsSpinner()
-
-        # spinner.start_spin('Reading in all info')
-        # start = time.time()
         all_data = self.read_excel(self.folder_root / (self.file_name + '.xlsx'), self.file_sheet)
-        # end = time.time()
-        # spinner.stop_spin()
-        # print(f'({end - start:.3f}s)')
 
-        # spinner.start_spin('Writing info to output file')
-        # start = time.time()
         all_data.to_excel(out_dir / (out_file + '.xlsx'), index=False)
-        # end = time.time()
-        # spinner.stop_spin()
-        # print(f'({end - start:.3f}s)')
-
-        col_end = time.time()
-        print(f'Data collection successful ({col_end - col_start:.3f}s)')
