0550e2e423773deca0e9a783812b739b0d92602e
John Merkel <dev.genericallyterrible@gmail.com>
2022-06-23 17:18:03 -0700

    Added simple setup and venv support

---
+++ setup.ps1
@@ -0,0 +1,13 @@
+$BASE_DIR = $PSScriptRoot
+$VENV_DIR = Join-Path $BASE_DIR '.venv'
+$ACTIVATE_VENV = Join-Path $VENV_DIR 'Scripts/Activate.ps1'
+
+if (-not (Test-Path -PathType Container -Path $VENV_DIR)) {
+    Write-Host "Venv does not exist, creating"
+    python3 -m venv $VENV_DIR
+}
+
+. ($ACTIVATE_VENV)
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
