76c39a28170f84f73368b1e6eb4904a7142c830f
John Merkel <dev.genericallyterrible@gmail.com>
2022-08-25 08:35:03 -0700

    Added Rich progress bars

--- ArchiveCollector.py
+++ ArchiveCollector.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
+from multiprocessing.managers import DictProxy
 
 import pandas as pd
+from rich.progress import TaskID
 
 
 class ArchiveCollector(ABC):
@@ -8,7 +10,7 @@ class ArchiveCollector(ABC):
         self.folder_root = folder_root
 
     @abstractmethod
-    def collect(self, out_dir, out_file):
+    def collect(self, out_dir, out_file, progress: DictProxy, sub_task_id: TaskID):
         pass
 
     def read_excel(self, file_path, sheet_name=None):
@@ -37,21 +39,49 @@ class ArchiveComposer(ArchiveCollector):
         self.care_center_sheet = care_center_sheet
         self.address_sheet = address_sheet
 
-    def collect(self, out_dir, out_file):
+    def collect(self, out_dir, out_file, progress: DictProxy, sub_task_id: TaskID):
+        _upd = progress[sub_task_id]
+        _upd["total"] = 6
+        progress[sub_task_id] = _upd
+        steps_done = 0
         general = self.read_excel(
             self.folder_root / (self.general_file + ".xlsx"), self.general_sheet
         )
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         care_center = self.read_excel(
             self.folder_root / (self.care_center_file + ".xlsx"), self.care_center_sheet
         )
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         address = self.read_excel(
             self.folder_root / (self.address_file + ".xlsx"), self.address_sheet
         )
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         merged = general.merge(care_center, on="Provider UID", how="left").merge(
             address, on="Provider UID", how="left"
         )
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         merged.dropna(how="all", axis=1, inplace=True)
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         merged.to_excel(out_dir / (out_file + ".xlsx"), index=False)
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         return merged
 
 
@@ -61,10 +91,26 @@ class ArchiveCopier(ArchiveCollector):
         self.file_name = file_name
         self.file_sheet = file_sheet
 
-    def collect(self, out_dir, out_file):
+    def collect(self, out_dir, out_file, progress: DictProxy, sub_task_id: TaskID):
+        _upd = progress[sub_task_id]
+        _upd["total"] = 3
+        progress[sub_task_id] = _upd
+        steps_done = 0
         all_data = self.read_excel(
             self.folder_root / (self.file_name + ".xlsx"), self.file_sheet
         )
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         all_data.dropna(how="all", axis=1, inplace=True)
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         all_data.to_excel(out_dir / (out_file + ".xlsx"), index=False)
+        steps_done += 1
+        _upd["completed"] = steps_done
+        progress[sub_task_id] = _upd
+
         return all_data
--- Collect.py
+++ Collect.py
@@ -1,75 +1,165 @@
+# https://www.deanmontgomery.com/2022/03/24/rich-progress-and-multiprocessing/
+
+import multiprocessing
 import os
 import sys
 import time
-from multiprocessing import Pool
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing.managers import DictProxy
 from pathlib import Path
+
+from rich.progress import (
+    BarColumn,
+    Progress,
+    SpinnerColumn,
+    TaskID,
+    TaskProgressColumn,
+    TextColumn,
+    TimeElapsedColumn,
+)
 
 from ArchiveCollector import *
 from CollectorMap import ARCHIVE_COLLECTORS, Month
-from PromptSpinner import *
 
 
-class CollectorSignal(Enum):
-    DONE = 0
-
-
-def collect_with_stats(year, month, collection_path, overwrite_existing=False):
+def collect_with_stats(
+    year: int,
+    month: Month,
+    collection_path: Path,
+    progress: DictProxy,
+    sub_task_id: TaskID,
+    overwrite_existing: bool = False,
+):
     if year in ARCHIVE_COLLECTORS:
         year_dict = ARCHIVE_COLLECTORS[year]
         if month in year_dict:
             file_exists = os.path.isfile(collection_path / f"{year}-{month.value}.xlsx")
+            _upd = progress[sub_task_id]
             if (not file_exists) or (file_exists and overwrite_existing):
                 # Collected file does not exist
                 # Collect it using the mapping in CollectorMap
-                print(f"Collecting {year}-{month.value}")
-                month_start = time.time()
+
+                _upd["description"] = f"Collecting {year}-{month.value}"
+                progress[sub_task_id] = _upd
+
                 month_df = year_dict[month].collect(
-                    collection_path,
-                    f"{year}-{month.value}",
+                    collection_path, f"{year}-{month.value}", progress, sub_task_id
                 )
-                month_end = time.time()
-                print(
-                    f"Collected {year}-{month.value} ({month_end - month_start:.3f}s)"
-                )
+
             else:
                 # Collected file exists
-                print(f"Reading {year}-{month.value} to analyze columns")
+                _upd = progress[sub_task_id]
+                _upd["description"] = f"Reading {year}-{month.value} to analyze columns"
+                _upd["total"] = 1
+                progress[sub_task_id] = _upd
+
                 # Read in the columns of the archived month's data
                 month_df = pd.read_excel(
-                    collection_path / f"{year}-{month.value}.xlsx", nrows=0
+                    str(collection_path / (f"{year}-{month.value}.xlsx")), nrows=0
                 )
+                _upd["completed"] = 1
+                progress[sub_task_id] = _upd
 
             # Assemble column names into a dataframe
             cols = []
             for col in month_df.columns:
                 cols.append([col, year, month.value])
-            return pd.DataFrame(cols, columns=["Column Name", "Year", "Month"])
+
+            _upd["result"] = pd.DataFrame(
+                cols, columns=["Column Name", "Year", "Month"]
+            )
+            progress[sub_task_id] = _upd
 
 
 if __name__ == "__main__":
+
     collection_path = Path(os.path.dirname(os.path.realpath(sys.argv[0]))) / "collected"
     if not os.path.isdir(collection_path):
         os.mkdir(collection_path)
 
-    targets = []
-    overwrite = False
-    for year in ARCHIVE_COLLECTORS:
-        for month in ARCHIVE_COLLECTORS[year]:
-            targets.append(
-                [
-                    year,
-                    month,
-                    collection_path,
-                    overwrite,
-                ]
-            )
+    overwrite = True
 
-    print(f"Collecting archived data")
-    start_time = time.time()
-    with Pool() as pool:
-        dfs = pool.starmap(collect_with_stats, targets)
-    end_time = time.time()
-    print(f"Completed collection in {end_time-start_time:.3f}s")
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(),
+        TaskProgressColumn(),
+        "Elapsed:",
+        TimeElapsedColumn(),
+        refresh_per_second=2,  # Slower updates
+    ) as progress_bar:
+        futures = []  # Track jobs
+        with multiprocessing.Manager() as manager:
+            # Share state between main process and worker functions
+            _progress = manager.dict()
 
-    data = pd.concat(dfs, ignore_index=True)
-    data.to_csv(collection_path / "_column_stats.csv", index=False)
+            # Overall progress tracking
+            main_task = progress_bar.add_task("[bold]Collecting archived data...")
+
+            with ProcessPoolExecutor() as executor:
+                # Create processes and progress bars
+                for year in ARCHIVE_COLLECTORS:
+                    for month in ARCHIVE_COLLECTORS[year]:
+                        sub_task_id = progress_bar.add_task(
+                            f"Sub_Task {year}-{month.value}", visible=False, start=False
+                        )
+                        _progress[sub_task_id] = {
+                            "description": None,
+                            "completed": 0,
+                            "total": 0,
+                            "result": None,
+                        }
+                        futures.append(
+                            executor.submit(
+                                collect_with_stats,
+                                year,
+                                month,
+                                collection_path,
+                                _progress,
+                                sub_task_id,
+                                overwrite,
+                            )
+                        )
+
+                progress_bar.update(main_task, total=len(futures))
+
+                # Monitor progress
+                while True:
+                    n_completed = sum([future.done() for future in futures])
+
+                    progress_bar.update(main_task, completed=n_completed)
+
+                    for task_id, update_data in _progress.items():
+                        description = update_data["description"]
+                        completed = update_data["completed"]
+                        total = update_data["total"]
+                        if total != 0 and not progress_bar.tasks[task_id].started:
+                            progress_bar.start_task(task_id)
+                        progress_bar.update(
+                            task_id,
+                            description=description,
+                            completed=completed,
+                            total=total,
+                            visible=completed < total,
+                        )
+
+                    time.sleep(0.5)
+                    # All jobs completed, stop monitoring
+                    if n_completed == len(futures):
+                        break
+
+            # Raise any errors
+            for future in futures:
+                future.result()
+
+            data = None
+
+            for task_id, update_data in _progress.items():
+                df = update_data["result"]
+                if data is None:
+                    data = df
+                else:
+                    data = pd.concat([data, df], ignore_index=True)
+
+            if data is not None:
+                data.to_csv(collection_path / "_column_stats.csv", index=False)
