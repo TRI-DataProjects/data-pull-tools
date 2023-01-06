42e90d9fef8d8ca8a9aebcd908eb2d00b69a6d15
John Merkel <dev.genericallyterrible@gmail.com>
2023-01-06 15:24:26 -0800

    Added age detail utils

---
+++ age_detail_utils.py
@@ -0,0 +1,22 @@
+from pandas import DataFrame, MultiIndex
+
+from data_pull_tools.infer_index import CleaningInferrer
+
+
+def unstack_age_details(df: DataFrame) -> DataFrame:
+    inferrer = CleaningInferrer()
+    df = inferrer.infer_index(df)  # type: ignore
+    df = df.dropna(how="all").T.unstack(0).T  # type: ignore
+
+    # Fix multi-index
+    mi: MultiIndex = df.index.copy()  # type: ignore
+    names = list(mi.names)
+    names[-1] = "Age Details"
+    mi.rename(names, inplace=True)
+
+    # Convert "Record ID" to numeric
+    mi = mi.set_levels(mi.levels[0].astype(int), level=0)
+
+    df = df.set_index(mi)
+
+    return df
