4165d0b055d9d52b9720a0b24d83e36b18045523
John Merkel <dev.genericallyterrible@gmail.com>
2023-12-14 13:12:18 -0800

    Refactored collisions

--- data_pull_tools/toml_utils.py
+++ data_pull_tools/toml_utils.py
@@ -5,6 +5,7 @@ Includes functions to load TOML files, get or create tables, and update values.
 """
 from __future__ import annotations
 
+from collections.abc import Mapping
 from contextlib import contextmanager
 from dataclasses import dataclass
 from datetime import datetime, timezone
@@ -12,8 +13,6 @@ from functools import partial
 from os import PathLike
 from pathlib import Path
 from typing import TYPE_CHECKING, Literal, TypeVar
-
-from collections.abc import Mapping
 
 import tomlkit
 from tomlkit import TOMLDocument, table
@@ -31,6 +30,8 @@ StrPath: TypeAlias = str | PathLike[str]
 K = TypeVar("K")
 V = TypeVar("V")
 RecursiveMap = Mapping[K, V | "RecursiveMap[K, V]"]
+CollisionPolicy = Literal["raise", "replace"]
+DEFAULT_COLLISION_POLICY: CollisionPolicy = "replace"
 
 
 @contextmanager
@@ -79,11 +80,15 @@ def load_toml(
         return tomlkit.load(f)
 
 
+class NonTableKeyCollisionError(Exception):
+    ...
+
+
 def _toml_get_or_table(
     toml: Container | Table,
     key: str,
     *,
-    collisions: Literal["overwrite", "raise"] = "overwrite",
+    collisions: CollisionPolicy = "replace",
 ) -> Table:
     """Gets a table from the given key, creating if missing.
 
@@ -93,13 +98,18 @@ def _toml_get_or_table(
         The parent toml object.
     key : str
         The key to get or create the table at.
-    collisions : Literal["overwrite", "raise"], optional
-        How to handle collisions, by default "overwrite"
+    collisions : CollisionPolicy, default="replace"
+        How to handle collisions, by default "replace".
 
     Returns
     -------
     Table
         The table at the given key.
+
+    Raises
+    ------
+    NonTableKeyCollisionError
+        If `collisions` is "raise" and the key already exists but is not a table.
     """
     if key not in toml:
         value = table()
@@ -108,11 +118,13 @@ def _toml_get_or_table(
 
     value = toml[key]
     if not isinstance(value, Table):
+        # Handle collisions
         if collisions == "raise":
             msg = "Key '%s' already exists and is not a table."
-            raise ValueError(msg, key)
-        value = table()
-        toml[key] = value
+            raise NonTableKeyCollisionError(msg, key)
+        if collisions == "replace":
+            value = table()
+            toml[key] = value
     return value
 
 
@@ -120,7 +132,7 @@ def update_toml_values(
     toml: Container | Table,
     data: RecursiveMap[str, Any],
     *,
-    collisions: Literal["overwrite", "raise"] = "overwrite",
+    collisions: CollisionPolicy = "replace",
 ) -> Container | Table:
     """Updates the given toml values with the provided data.
     Creates intermediary tables as needed.
@@ -131,8 +143,8 @@ def update_toml_values(
         The toml object to update.
     data : RecursiveMap[str, Any]
         The data to update the toml with.
-    collisions : Literal["overwrite", "raise"], optional
-        How to handle key collisions, by default "overwrite"
+    collisions : CollisionPolicy, default="replace"
+        How to handle key collisions, by default "replace"
 
     Returns
     -------
@@ -148,6 +160,53 @@ def update_toml_values(
             continue
         toml[key] = value
     return toml
+
+
+def update_toml_file(
+    toml_path: StrPath | Path,
+    data: RecursiveMap[str, Any],
+    *,
+    collisions: CollisionPolicy = "replace",
+) -> None:
+    """Opens a toml file, updates its contents with the provided data, and saves it.
+    Creates intermediary tables as needed.
+
+    Parameters
+    ----------
+    toml_path : StrPath | Path
+        The path to the toml file to update.
+    data : RecursiveMap[str, Any]
+        The data to update the toml with.
+    collisions : CollisionPolicy, default="replace"
+        How to handle data key collisions, by default "replace"
+    """
+    with manage_toml_file(toml_path) as toml_file:
+        update_toml_values(toml_file, data, collisions=collisions)
+
+
+def update_toml_value(
+    toml: Container | Table,
+    key_chain: list[str],
+    value: Any,  # noqa: ANN401
+    *,
+    collisions: CollisionPolicy = "replace",
+) -> Container | Table:
+    current = toml
+    for key in key_chain[:-1]:
+        current = _toml_get_or_table(current, key, collisions=collisions)
+    current[key_chain[-1]] = value
+    return toml
+
+
+def update_toml_file_value(
+    toml_path: StrPath | Path,
+    key_chain: list[str],
+    value: Any,  # noqa: ANN401
+    *,
+    collisions: CollisionPolicy = "replace",
+) -> None:
+    with manage_toml_file(toml_path) as toml_file:
+        update_toml_value(toml_file, key_chain, value, collisions=collisions)
 
 
 @dataclass
